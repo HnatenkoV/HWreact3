e9595b9a4e81360e753349f5d577e29c9de77e42
Hnatenkov <hnatenko.vadim3@gmail.com>
2023-03-18 12:04:30 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useContext} from "react";
+import CustomEdit from "./components/customInput";
+import {ThemeContext} from "./providers/themeProvider";
 
 function App() {
+    const [isFieldInvalid, setIsFieldInvalid] = useState(false)
+    const themeCtx = useContext(ThemeContext);
+
+    const handleCustomChange = (value) => {
+        if(!isNaN(value) && value !== '') {
+            setIsFieldInvalid(false);
+        }else {
+            setIsFieldInvalid(true)
+        }
+    }
+
+    const handleThemeChangeLight = () => {
+        themeCtx.changeThemeL()
+    }
+    const handleThemeChangeDark = () => {
+        themeCtx.changeThemeD()
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="first-div">
+        <button className="btn" onClick={handleThemeChangeLight}>Light</button>
+        <button className="btn" onClick={handleThemeChangeDark}>Dark</button>
+        </div>
+        <div className={themeCtx.theme ? 'Theme-dark' : 'Theme-light'}>
+            <div className="theme-ind">
+                <p>{themeCtx.theme ? 'Night' : 'Day'}</p>
+            </div>
+            <div className="input-block">
+            <CustomEdit text="" placeHolder="some text_1" onChange={handleCustomChange} isNotValid={isFieldInvalid}/>
+            <CustomEdit text="" placeHolder="some text_2" onChange={handleCustomChange} isNotValid={isFieldInvalid}/>
+            </div>
+        </div>
     </div>
   );
 }
